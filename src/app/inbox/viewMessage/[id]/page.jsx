9f2c84c0ce0269825fb61faf86ae54ce5151a186
sistemastnu <sistemas@tnuniversity.edu.mx>
2024-12-12 09:24:45 -0700

    changes

--- src/app/inbox/viewMessage/[id]/page.jsx
+++ src/app/inbox/viewMessage/[id]/page.jsx
@@ -18,61 +18,61 @@ const ViewMessage = ({ params }) => {
   const id = params.id;
   const { data } = useSWR(`/api/inbox/${id}`, fetcher);
   const [response, setResponse] = useState("");
+  const [editorData, setEditorData] = useState("");
+
   if (!data) return <Loader />;
   const mostrarBodyConSaltosDeLinea = (texto) => {
     return texto.split("\n").map((linea, index) => <p key={index}>{linea}</p>);
   };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setResponse(value);
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(response);
-    // const responseData = {
-    //   message_id: id,
-    //   body: response.response,
-    // };
-    // await fetch(`/api/inbox/reply`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(responseData),
-    // });
-    // setResponse("");
-    // alert("Message sent successfully");
-    // window.location.href = "/inbox";
   };
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName={"View Message"} />
-      <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-        <div className="bg-white p-6 rounded-lg shadow-sm mb-4">
-          <p className="text-black text-bold mb-4">
-            From: <strong>{data.sender}</strong>
-            <br />
-            Phone: <strong>{data.phoneNumber}</strong>
-            <br />
-            Name: <strong>{data.firstName}</strong>
-            <br />
-            Last Name: <strong>{data.lastName}</strong>
-          </p>
-          <p className="text-black text-bold mb-4"></p>
-          <div className="space-y-4 text-base text-black">
-            {mostrarBodyConSaltosDeLinea(data.body)}
-          </div>
-        </div>
-        <div className="bg-white p-6 rounded-lg shadow-sm">
-          <h3 className="text-lg font-semibold mb-4">Reply</h3>
-          <RichText />
-          <div>
-            <button
-              onClick={handleSubmit}
-              className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-500 transition-colors"
-            >
-              Send
-            </button>
+      <div className="grid grid-cols-3 gap-8">
+        <div className="col-span-5 xl:col-span-3">
+          <div className="rounded-lg border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <div className="bg-white p-6 rounded-lg shadow-sm mb-4">
+              <p className="text-black text-bold mb-4">
+                From: <strong>{data.sender}</strong>
+                <br />
+                Phone: <strong>{data.phoneNumber}</strong>
+                <br />
+                Name: <strong>{data.firstName}</strong>
+                <br />
+                Last Name: <strong>{data.lastName}</strong>
+              </p>
+              <p className="text-black text-bold mb-4"></p>
+              <div className="space-y-4 text-base text-black">
+                {mostrarBodyConSaltosDeLinea(data.body)}
+              </div>
+            </div>
+            <div className="bg-white p-6 rounded-lg shadow-sm">
+              <h3 className="text-lg font-semibold mb-4">Reply</h3>
+              <div className="relative max-w-full ">
+                <RichText
+                  editorData={editorData}
+                  setEditorData={setEditorData}
+                />
+              </div>
+              <div>
+                <button
+                  onClick={handleSubmit}
+                  className="bg-orange-400 text-white px-4 py-2 rounded-md hover:bg-orange-500 transition-colors"
+                >
+                  Send
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
